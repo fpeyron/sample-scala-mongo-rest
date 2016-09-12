a5a1339ce2388ff571ff9b356d52d294d92af0cd
zbian <zhuhan.bian@betc.com>
2016-09-12 16:44:06 +0200

    ajoute mongo client options config

--- src/main/scala/fr/sysf/sample/Application.scala
+++ src/main/scala/fr/sysf/sample/Application.scala
@@ -1,8 +1,13 @@
 package fr.sysf.sample
 
+import com.mongodb.MongoClientOptions
+import com.mongodb.MongoClientOptions.Builder
+import fr.sysf.sample.config.MongoOptionProperties
 import fr.sysf.sample.domain.Hotel
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
 import org.springframework.data.mongodb.config.EnableMongoAuditing
@@ -30,4 +35,32 @@ class RestMvcConfig {
     }
   }
 
+}
+
+@Configuration
+@EnableConfigurationProperties(Array(classOf[MongoOptionProperties]))
+class MongoDbConfig {
+
+  @Autowired
+  private val mongoOptionProperties: MongoOptionProperties = null
+
+  @Bean
+  def mongoClientOptions(): MongoClientOptions = {
+    val builder: Builder = MongoClientOptions.builder()
+
+    if (mongoOptionProperties.connectionsPerHost > 0) {
+      builder.connectionsPerHost(mongoOptionProperties.connectionsPerHost)
+    }
+    if (mongoOptionProperties.threadsAllowedToBlockForConnectionMultiplier > 0) {
+      builder.threadsAllowedToBlockForConnectionMultiplier(mongoOptionProperties.threadsAllowedToBlockForConnectionMultiplier)
+    }
+    if (mongoOptionProperties.connectTimeout > 0) {
+      builder.connectTimeout(mongoOptionProperties.connectTimeout)
+    }
+    if (mongoOptionProperties.socketTimeout > 0) {
+      builder.socketTimeout(mongoOptionProperties.socketTimeout)
+    }
+
+    builder.build()
+  }
 }
