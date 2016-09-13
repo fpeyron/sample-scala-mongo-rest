e2a3e118f9084f40c7c2253cb1b6c889fe732d94
Florent Peyron <florent.peyron@ext.betc.com>
2016-09-13 10:58:58 +0200

    ARCH : add JavaDateConvertor for Json

--- src/main/scala/fr/sysf/sample/Application.scala
+++ src/main/scala/fr/sysf/sample/Application.scala
@@ -1,13 +1,17 @@
 package fr.sysf.sample
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper, SerializationFeature}
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import fr.sysf.sample.domain.Hotel
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
 import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
-import org.springframework.data.rest.webmvc.config.{RepositoryRestConfigurer, RepositoryRestConfigurerAdapter}
-import org.springframework.validation.Validator
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
@@ -22,21 +26,33 @@ class ApplicationConfig
 
 
 @Configuration
-class RestMvcConfig extends WebMvcConfigurerAdapter{
+class RestMvcConfig extends WebMvcConfigurerAdapter {
 
   @Bean
-  def repositoryRestConfigurer(): RepositoryRestConfigurer = new RepositoryRestConfigurerAdapter() {
+  def repositoryRestConfigurer() = new RepositoryRestConfigurerAdapter() {
     override def configureRepositoryRestConfiguration(config: RepositoryRestConfiguration) {
       config.exposeIdsFor(classOf[Hotel])
     }
   }
 
-  @Bean
-  def validator(): LocalValidatorFactoryBean = {
-    new LocalValidatorFactoryBean()
+
+  override def configureMessageConverters(converters: java.util.List[HttpMessageConverter[_]]) {
+    val converter = new MappingJackson2HttpMessageConverter()
+    converter.setObjectMapper(
+      new ObjectMapper()
+        .registerModule(new JavaTimeModule())
+        .setSerializationInclusion(Include.NON_EMPTY)
+        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .findAndRegisterModules())
+    converters.add(converter)
   }
 
-  override def getValidator: Validator = {
-    validator()
-  }
+
+  @Bean
+  def validator() = new LocalValidatorFactoryBean()
+
+
+  override def getValidator = validator()
+
 }
